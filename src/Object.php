484cfe977ca16e14a91ed1dd106335585099c0f1
Roukmoute <contact@roukmoute.fr>
2017-09-26 23:13:20 +0200

    Create Object class

---
+++ src/Object.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace PHPSystem\Core;
+
+/**
+ * Supports all classes in the PHPSystem class hierarchy and provides
+ * low-level services to derived classes.
+ *
+ * This is the ultimate base class of all classes in the PHPSystem;
+ * it is the root of the type hierarchy.
+ */
+class Object
+{
+}
