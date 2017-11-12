6bdfb2ac80aa0ed1336f98547b5e967bc403d7ab
Roukmoute <contact@roukmoute.fr>
2017-11-12 16:11:49 +0100

    Add FormatException class

---
+++ src/Exception/FormatException.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace PHPSystem\Core\Exception;
+
+/**
+ * Exception to designate an illegal argument.
+ */
+class FormatException extends \InvalidArgumentException
+{
+}
